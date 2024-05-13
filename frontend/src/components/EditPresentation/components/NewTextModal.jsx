4ef9bdf521590d1a8d83a65a732a41587681a18b
Baqir <baqir@Baqirdendiannao.lan>
2024-05-13 17:14:23 +1000

    3

--- frontend/src/components/EditPresentation/components/NewTextModal.jsx
+++ frontend/src/components/EditPresentation/components/NewTextModal.jsx
@@ -1,39 +1,44 @@
 import React, { useState } from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Slide } from '@mui/material';
 
-function NewTextModal ({ open, onClose, onTextChange, onTextSizeChange, onTextColorChange, onAddText }) {
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
+const Transition = React.forwardRef(function Transition (props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+function NewTextModal ({ open, onClose, onAddText }) {
+  const [text, setText] = useState('');
+  const [textSize, setTextSize] = useState(16);
+  const [textColor, setTextColor] = useState('#000000');
+  const [width, setWidth] = useState(100);
+  const [height, setHeight] = useState(100);
 
   const handleCreate = () => {
-    onCreate(name, description);
+    onAddText(text, textSize, textColor, width, height);
     onClose();
   };
 
   return (
-    <Dialog open={open} onClose={onClose}>
+    <Dialog open={open} onClose={onClose} TransitionComponent={Transition} >
       <DialogTitle>add text</DialogTitle>
       <DialogContent>
         <TextField
           autoFocus
           margin="dense"
-          label="presentation name"
+          label="text content"
           type="text"
           fullWidth
           variant="standard"
           value={name}
-          onChange={e => setName(e.target.value)}
+          onChange={e => setText(e.target.value)}
         />
         <TextField
           margin="dense"
-          label="presentation description"
-          type="text"
+          label="text size"
+          type="number"
           fullWidth
-          variant="standard"ds
-          value={description}
-          onChange={e => setDescription(e.target.value)}
-          multiline
-          rows={4}
+          variant="standard"
+          value={textSize}
+          onChange={e => setTextSize(e.target.value)}
         />
       </DialogContent>
       <DialogActions>
