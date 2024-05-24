3bc7d982fa17b61457cd6024d2c8e3c3c3d52635
Baqir <baqir@Baqirdebijibendiannao.local>
2024-05-24 15:03:05 +1000

    fix video element

--- frontend/src/components/EditPresentation/components/DraggableElement.jsx
+++ frontend/src/components/EditPresentation/components/DraggableElement.jsx
@@ -53,12 +53,15 @@ export default function DraggableElement ({ element }) {
       {element.type === 'image' && (
         <img src={element.url} alt={element.description} style={{ width: '100%', height: '100%' }} />
       )}
-      {element.type === 'video' && (
-        <video
-          src={element.url}
-          autoPlay={element.autoPlay}
-          style={{ width: '100%', height: '100%' }}
-        />
+      {element.type === 'video' && element.url.includes('youtube.com') && (
+        <iframe
+          width="90%"
+          height="90%"
+          src={`https://www.youtube.com/embed/${element.url.split('v=')[1]}`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
       )}
     </Box>
   )
